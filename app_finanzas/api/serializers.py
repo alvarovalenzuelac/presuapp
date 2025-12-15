788498911b08c1665fb3a4b611ce427befce1303
Alvaro Valenzuela <alvaro.inthespiral@gmail.com>
2025-12-15 20:54:06 -0300

    Se aplican validaciones a la api, por categorias.

--- app_finanzas/api/serializers.py
+++ app_finanzas/api/serializers.py
@@ -1,11 +1,39 @@
 from rest_framework import serializers
 from app_finanzas.models import Categoria, Transaccion, Presupuesto
+from django.db.models import Q
 
 # 1. SERIALIZER DE CATEGORÍAS
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = ['id', 'nombre', 'icono', 'categoria_padre', 'usuario']
+        read_only_fields = ['usuario']
+    def validate(self, data):
+        # Validar duplicados antes de crear/actualizar
+        user = self.context['request'].user
+        nombre = data.get('nombre')
+        categoria_padre = data.get('categoria_padre')
+        
+        # 1. Validación: No permitir crear categorías Padre (padre=None)
+        # Si es una creación (no tiene instancia) y no tiene padre...
+        if not self.instance and not categoria_padre:
+             raise serializers.ValidationError({"non_field_errors": ["Solo puedes crear subcategorías dentro de las existentes."]})
+
+        # 2. Validación: Nombre Duplicado en el mismo Padre
+        query = Categoria.objects.filter(
+            nombre__iexact=nombre,
+            categoria_padre=categoria_padre,
+            usuario=user
+        )
+        
+        # Si estamos editando, nos excluimos a nosotros mismos de la búsqueda
+        if self.instance:
+            query = query.exclude(pk=self.instance.pk)
+
+        if query.exists():
+            raise serializers.ValidationError({"nombre": [f"Ya tienes una categoría llamada '{nombre}' en este grupo."]})
+
+        return data
 
 # 2. SERIALIZER DE TRANSACCIONES
 class TransaccionSerializer(serializers.ModelSerializer):
